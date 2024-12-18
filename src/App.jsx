49bea3362950666ab1fa9dcfa7ee463426c1a5cd
shaheed <shaheedshanu100@gmail.com>
2024-12-19 00:08:42 +0530

    Fix routing for GitHub Pages deployment by adding basename

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,22 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home'
-import { Route, Routes } from 'react-router-dom'
 import About from './Pages/About'
 import Service from './Pages/Service'
 import Work from './Pages/Work'
 import Contact from './Pages/Contact'
-// import Navbar from './components/Navbar'
-
 
 const App = () => {
   return (
-    <>
-    
-      {/* <Navbar/> */}
-     <Routes>
-         <Route path='/' element={<Home/>}/>
-         <Route path='/about' element={<About/>}/>
-         <Route path='/service' element={<Service/>}/>
-         <Route path='/work' element={<Work/>}/>
-         <Route path='/contact' element={<Contact/>}/>
-     </Routes>
-    </>
+    <Router basename="/portfolio/">  {/* Set the base URL for your app */}
+      <Routes>
+         <Route path='/' element={<Home />} />
+         <Route path='/about' element={<About />} />
+         <Route path='/service' element={<Service />} />
+         <Route path='/work' element={<Work />} />
+         <Route path='/contact' element={<Contact />} />
+      </Routes>
+    </Router>
   )
 }
 
